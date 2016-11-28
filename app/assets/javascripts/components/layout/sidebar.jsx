35cb6bc650261f16b7c34e0a5a539b3a4c1e074e
Thomas Chrétien <thomas@chretien.co>
2016-11-28 15:29:13 +0700

    petite pause

--- app/assets/javascripts/components/layout/sidebar.jsx
+++ app/assets/javascripts/components/layout/sidebar.jsx
@@ -47,7 +47,7 @@ class Sidebar extends React.Component {
           })}
         </div>
         { this.props.user_signed_in ?
-          <SidebarMessage title={'Hubum'} description={"is on desktop! Download, connect your account and your in."} url={'/download/desktop'} cta={'Download the app'} classes='download-desktop' />
+          false && <SidebarMessage title={'Hubum'} description={"is on desktop! Download, connect your account and your in."} url={'/download/desktop'} cta={'Download the app'} classes='download-desktop' />
         :
           <SidebarMessage title={'Hubum'} description={"is a private network of music libraries."} url={"http://app.hubum.com"} cta={'Create a library'} />
         }
